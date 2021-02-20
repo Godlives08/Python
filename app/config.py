9d1a630153a242275dbf5c09b7cd88876ddaba4c
Godlives08 <71352638+Godlives08@users.noreply.github.com>
2021-02-20 14:42:38 -0400

    V01

--- app/config.py
+++ app/config.py
@@ -4,10 +4,9 @@ import MySQLdb.cursors
 import re
 
 app = Flask(__name__)
-# Change this to your secret key (can be anything, it's for extra protection)
+
 app.secret_key = 'sklejdyriwkyfaioskdgfokdghfpiuyedpifsdhfpkj'
 
-# Enter your database connection details below
 app.config['MYSQL_HOST'] = 'localhost'
 app.config['MYSQL_USER'] = 'python'
 app.config['MYSQL_PASSWORD'] = 'python2021'
